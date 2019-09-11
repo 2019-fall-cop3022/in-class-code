ba1b80b46bd65c9da47d4ddbd10835c79c1a310e
Steven Bitner <sbitner@uwf.edu>
2019-09-11 15:16:12 -0500

    Adding parameter setting to constructors

--- Week_3/src/Car.java
+++ Week_3/src/Car.java
@@ -1,11 +1,20 @@
 
 public class Car {
+	private String color;
+	private Transmission transmission;
+	
 	public Car ()
 	{
-		
+		this("black", Transmission.AUTOMATIC);
 	}
 	public Car (String color, Transmission t)
 	{
-		
+		this.color = color;
+		transmission = t;
+	}
+	
+	public String toString ()
+	{
+		return this.color;
 	}
 }
--- Week_3/src/CarTest.java
+++ Week_3/src/CarTest.java
@@ -7,7 +7,10 @@ class CarTest {
 	@Test
 	void test() {
 		Car jalopy = new Car();
+		assertEquals("black", jalopy.toString());
 		Car sportsCar = new Car("red", Transmission.MANUAL);
+		assertEquals("red", sportsCar.toString());
+		
 	}
 
 }
