4c5ecd51c1bec87842b28ffc3a824487ca4e1897
Steven Bitner <sbitner@uwf.edu>
2019-09-25 14:54:32 -0500

    Adding simple toString example

---
+++ Week_4/src/Item.java
@@ -0,0 +1,15 @@
+
+public class Item {
+// data
+	// methods
+	public static void main (String[] args)
+	{
+		Item i = new Item();
+		System.out.println(i);
+	}
+	
+	public String toString ()
+	{
+		return "foo";
+	}
+}
